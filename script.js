ee1a6a4a74dcb663669b92a76eb62a622318595f
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2022-10-28 19:32:51 -0600

    Updated JS, meets all  acceptance criteria with no errors, restructured js and added guaranteed array

--- script.js
+++ script.js
@@ -1,68 +1,120 @@
-// Assignment code here
+// activates generate as a button
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
+
+// Assignment code here
 // this generates an array
 // of the alphabet in all uppercase.
-const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+var alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphaUpper = alpha.map((x) => String.fromCharCode(x));
 const alphaLower = [];
-
-// declare constantvariable with the value of an
-// array that contains string of each number on the kaypad
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-// declare constant variable with the value of an array containing all of the special characters.
-
-const special =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", "'", ":", "|", ".", "<", ">", "~"];
-
 // this pushes a complete lowercase aplhabet
 // to the alphaLower array one element at a time.
 alphaUpper.forEach(element => {
   alphaLower.push(element.toLowerCase());
 });
+// declare constantvariable with the value of an
+// array that contains string of each number on the kaypad
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+// declare constant variable with the value of an array containing all of the special characters.
+const special =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", "'", ":", "|", ".", "<", ">", "~"];
 // proof the const variable have been updated and print
 // to console correctly..
-console.log(alphaUpper);
-console.log(alphaLower);
-console.log(special);
-console.log(numbers);
-// successful!!
-
-// Create gereatePassword() function: 
-function generatePassword() {
-  // first step of generation is prompting
-  // the user for the password length &
-  // checking it meets acceptability criteria:
-  // between 8 and 128 characters.
-  // use local variables to define needed check values
-  var passLength = prompt("Please enter the number of desired characters for you new password.");
-
-}
+// console.log(alphaUpper);
+// console.log(alphaLower);
+// console.log(special);
+// console.log(numbers);
+// successful!! all arrays are printing properly..
 
 
-// Get references to the #generate element
-// declaring variable generateBtn with the value
-// asssigned to the element with the assignment 
-// id="generate", which is the main application's button.
-var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
-// this is a test to see output of the
-// writePassword function
-console.log(password);
-console.log(passwordText);
-// looks like generatePassword() function is undefined.
-// create the generate password function at top of file. 
 
+function generatePassword() {
+  var passLength = prompt("Choose a length of your password, between 8 and 128 characters.");
+  passLength = Number(passLength);
+  console.log(passLength);
+
+  if (passLength < 8 || passLength > 128) {
+    alert("Password length must be between 8 and 128 characters");
+    return null;
+  }
+
+  // need checks to include certain characters or not
+  var wantUpper = confirm("Would you like to use UPPERCASE in your password.");
+  console.log(wantUpper);
+
+  var wantLower = confirm("Please confirm if you'd like lower cases in your password.");
+  console.log(wantLower);
+
+  var wantNum = confirm("Please confirm if you'd like numbers in your password.");
+  console.log(wantNum);
+
+  var wantSpecial = confirm("Please confirm if you'd like special characters in your password.");
+  console.log(wantSpecial);
+
+  // creating new varChars to embody if the user choses okay or not on criteria. This is written to say if it's true uppperCase pull from that array and continues until completed.
+  var newChars = [];
+  var guarenteedChars = [];
+
+  // checks the confirm values in variable to be truthy to push random element 
+  // if truthy
+  if (wantUpper) {
+    newChars = newChars.concat(alphaUpper);
+    // pick a random element from selected array set and pushes that element to the guaranteed array
+    var randomUpper = Math.floor(Math.random() * alphaUpper.length);
+    guarenteedChars.push(alphaUpper[randomUpper]);
+  }
+//  if truthy
+  if (wantLower) {
+    newChars = newChars.concat(alphaLower);
+    // sends a single random element from alphaLower array the guaranteed array 
+    var randomLower = Math.floor(Math.random() * alphaLower.length);
+    guarenteedChars.push(alphaLower[randomLower]);
+  }
+  // if truthy
+  if (wantNum) {
+    newChars = newChars.concat(numbers);
+    // sends a random element from numbers array to guaranteed array
+    var randomNum = Math.floor(Math.random() * numbers.length);
+    guarenteedChars.push(numbers[randomNum]);
+  }
+  // if truthy
+  if (wantSpecial) {
+    newChars = newChars.concat(special);
+    // send a random element from special array this array to the guaranteed array 
+    var randomSpec = Math.floor(Math.random() * special.length);
+    guarenteedChars.push(special[randomSpec]);
+  }
+  // shows array of guaranteed characters
+  console.log(guarenteedChars);
+
+  // create final password:
+  var finalPassword = [];
+
+  // create random password using for loop
+  for (var i = 0; i < passLength - guarenteedChars.length; i++) {
+    var randomNumber = Math.floor(Math.random() * newChars.length);
+    finalPassword = finalPassword.concat(newChars[randomNumber]);
+  }
+
+  // this guarentees all selected criteria is at the end of the password.
+  finalPassword = finalPassword.concat(guarenteedChars);
+
+  // omits ray elements seperated by periods is what join does
+  finalPassword = finalPassword.join("");
+  console.log(newChars);
+  console.log(finalPassword);
+  return finalPassword;
+}
 
 
 
